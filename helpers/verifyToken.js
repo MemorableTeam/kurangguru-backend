a8cf4f6aa7fdcebde2598f21306a432bfd85d1d3
rpetzandrian <rpetzandrian@gmail.com>
2021-06-07 14:14:45 +0700

    varify token

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -1,35 +1,38 @@
 const jwt = require('jsonwebtoken');
-const formResponse = require('./formResponse');
 
-const verifyToken = (req, res, next) => {
-    const bearerToken = req.header('token')
-    if (!bearerToken) {
-        formResponse({
-            message: `Resource not Found`,
-            status: 404,
-        }, res)
-    }else{
-        jwt.verify(bearerToken, process.env.SECRET_KEY, function (err, decoded) {
-            if (!err) {
-                if(decoded.role == 'admin') next()
-                else if(decoded.role == 'member') next()
-                else if(decoded.role == 'fasilitator') next()
-                else if (decoded.id == req.query.id) next()
-                /* else formResponse({
-                    message: `Forbidden`,
-                    status: 403
-                }, res) */
-                else  res.status(403).send({message : 'Forbidden'})
-            } else {
-                res.status(400).send({message : `${err.message},${bearerToken}`})
-               /*  formResponse({
-                    message: ,
-                    status: 400,
-                }, res) */
-            }
-        });
-    }
+const verifyToken = {
+	verifyId: (req, res, next) => {
+		const bearerToken = req.headers.authorization
+		if (!bearerToken) {
+			res.status(404).send({ message: 'Resource not found!', status: 404 })
+		} else {
+			jwt.verify(bearerToken.split(' ')[1], process.env.SECRET_KEY, function (err, decoded) {
+				if (!err) {
+					req.query.role = decoded.role
+					if (decoded.id == req.query.user_id) next()
+					else res.status(403).send({ message: 'Forbidden', status: 403 })
+				} else {
+					res.status(400).send({ message: `${err.message},${bearerToken}`, status: 400 })
+				}
+			});
+		}
+	},
+
+	verifyFasilitator: (req, res, next) => {
+		const bearerToken = req.headers.authorization
+		if (!bearerToken) {
+			res.status(404).send({ message: 'Resource not found!', status: 404 })
+		} else {
+			jwt.verify(bearerToken.split(' ')[1], process.env.SECRET_KEY, function (err, decoded) {
+				if (!err) {
+					if (decoded.role == 'fasilitator') next()
+					else res.status(403).send({ message: 'Forbidden', status: 403 })
+				} else {
+					res.status(400).send({ message: `${err.message},${bearerToken}`, message: 400 })
+				}
+			});
+		}
+	},
 }
-
 
 module.exports = verifyToken
