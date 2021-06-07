69a0eda453c4a9eb5712fa30ed53effdeb476cb0
= <=>
2021-06-07 13:31:20 +0700

    change role verify member to user

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -12,7 +12,7 @@ const verifyToken = (req, res, next) => {
         jwt.verify(bearerToken, process.env.SECRET_KEY, function (err, decoded) {
             if (!err) {
                 if(decoded.role == 'admin') next()
-                else if(decoded.role == 'member') next()
+                else if(decoded.role == 'user') next()
                 else if(decoded.role == 'fasilitator') next()
                 else if (decoded.id == req.query.id) next()
                 /* else formResponse({
