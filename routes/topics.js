e519e0a47eaad37c1fab36607083d9040d509f76
= <=>
2021-06-08 13:31:58 +0700

    merger and on going

--- routes/topics.js
+++ routes/topics.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router();
 const topicsController = require('../controllers/topics');
 const { verifyId, onlyReturnRole, verifyFasilitator } = require('../helpers/verifyToken');
-
-
 router.get('/',onlyReturnRole,topicsController.getTopics)
 router.post('/add',topicsController.addTopics)
 
