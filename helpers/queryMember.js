93f860f04056e0aa0371e0a97ba80555715eaeec
rpetzandrian <rpetzandrian@gmail.com>
2021-06-09 20:31:15 +0700

    fixing double data in get by id

--- helpers/queryMember.js
+++ helpers/queryMember.js
@@ -15,7 +15,7 @@ const queryMember = {
     let query = `SELECT a.*, b.score , c.user_id, c.username, c.photo
         FROM topics AS a
         LEFT JOIN (SELECT a.* FROM score AS a INNER JOIN users AS b ON a.user_id = b.id WHERE b.id = ${request.user_id}) AS b ON b.topic_id = a.id
-        left join (select a.id, b.id as user_id, b.username, b.photo, a.class_id from user_class as a inner join users as b on b.id = a.user_id where a.class_id = ${request.class_id}) as c on c.class_id = a.id_class
+        left join (select a.id, b.id as user_id, b.username, b.photo, a.class_id from user_class as a inner join users as b on b.id = a.user_id where a.class_id = ${request.class_id} and a.user_id = ${request.user_id}) as c on c.class_id = a.id_class
         WHERE a.id_class = ${request.class_id} 
         ORDER BY a.id ASC`
 
